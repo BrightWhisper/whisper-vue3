7bff1f63b73a2e860eb4281d4ec2dad5d238f95c
brightwhisper <sureforwsy@163.com>
2021-08-29 02:02:29 +0800

    封装topnav组件

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Topnav />
+        <div class="banner">
+            <h1>UI</h1>
+            <h2>一个UI框架</h2>
+            <p class="actions">
+                <a href>GitHub</a>
+                <a href>开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { reactive } from 'vue';
+import Topnav from '../components/Topnav.vue';
+export default {
+    components: { Topnav }
+}
+const state = reactive({});
+
+</script>
+<style lang="scss" scoped>
+</style>
